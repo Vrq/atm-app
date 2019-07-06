ce1764234698dd31b09c8be9c24bf19fff1f6234
Janusz Tomasik <januszveiro@gmail.com>
2019-07-06 22:30:49 +0200

    cleaning up server side

--- server/controllers/atms.ctrl.js
+++ server/controllers/atms.ctrl.js
@@ -1,26 +1,22 @@
 const fetch = require('isomorphic-unfetch')
 
 module.exports = {
-    getAtmsWithinRadius: (req, res) => {
+    getAtmsWithinRadius: async (req, res) => {
         const METERS_IN_MILE = 1609.34
         const long = req.params.longitude
         const lat = req.params.latitude
         const radiusInMeters = req.params.radius
-        const radiusInFullMiles = Math.floor(radiusInMeters/METERS_IN_MILE)
-        console.log("full miles" + radiusInFullMiles)
-        fetch("https://api.hsbc.com/x-open-banking/v2.1/atms/geo-location/lat/" + lat + "/long/" + long + "?radius=" + radiusInFullMiles, {
-            method: 'GET',
-            headers: { "Content-Type": "application/json" }
-        }).then((atmResponse) => {
-            if (atmResponse.ok) {
-                atmResponse.json().then(atmResponseBody => {
-                    res.send(atmResponseBody)
-                });
-            } else {
-                console.log("Failed to fetch atms")
-            }
-        })
+        const radiusInFullMiles = Math.floor(radiusInMeters / METERS_IN_MILE)
+        
+        const HSBC_API_URI = "https://api.hsbc.com/x-open-banking/v2.1/atms/geo-location/lat/" + lat + "/long/" + long + "?radius=" + radiusInFullMiles
 
-
+        const atmResponse = await fetch(HSBC_API_URI)
+        if (atmResponse.ok) {
+            const atmResponseBody = await atmResponse.json()
+            res.status(200).send(atmResponseBody)
+        } else {
+            console.log("Failed to ATM data")
+            res.status(500).send("Problem with fetching ATM data")
+        }
     }
 };
